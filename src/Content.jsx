ce4d916aa35b6e8e23dd2e507a16cda7a939a8a2
Krishna Pradhan <kpaccess@gmail.com>
2023-03-25 17:39:50 -0400

    initial commit

---
+++ src/Content.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+const Content = () => {
+  // const [searchText, setSearchText] = useState("");
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
+  return (
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" checked={item.checked} />
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default Content;
