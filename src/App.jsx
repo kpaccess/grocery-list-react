2487fe43d7ace8acce56015d9098cd5dc7dd8a66
Krishna Pradhan <kpaccess@gmail.com>
2023-03-25 20:23:07 -0400

    removed unused state, added header props, updated main css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Header from "./Header";
-import Content from "./Content";
-import Footer from "./Footer";
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <Header />
+      <Header title="Groceries List" />
       <Content />
       <Footer />
     </div>
