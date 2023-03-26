a38ddd9ea05bbc3d3985d1eed8dcff3b21087ef4
Krishna Pradhan <kpaccess@gmail.com>
2023-03-26 12:11:47 -0400

    Added seach component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 import AddItem from './AddItem';
+import SearchItem from './SearchItem';
 
 function App() {
   const [items, setItems] = useState(
@@ -11,6 +12,7 @@ function App() {
     ) || []
   );
   const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
 
   const reuseLocalStorage = (item) => {
     setItems(item);
@@ -55,16 +57,35 @@ function App() {
     setNewItem('');
   };
 
+  let data;
+
+  console.log(
+    'items ',
+    items.filter((item) =>
+      item.item
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    )
+  );
+
   return (
     <div className="App">
       <Header title="Groceries List" />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
       <Content
-        items={items}
+        items={items.filter((item) =>
+          item.item
+            .toLowerCase()
+            .includes(search.toLowerCase())
+        )}
         setItems={setItems}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
---
+++ src/SearchItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SearchItem = ({ search, setSearch }) => {
+  return (
+    <form
+      className="searchForm"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <label htmlFor="search">Search</label>
+      <input
+        type="search"
+        role="searchbox"
+        placeholder="Search Items"
+        id="search"
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+      />
+    </form>
+  );
+};
+
+export default SearchItem;
